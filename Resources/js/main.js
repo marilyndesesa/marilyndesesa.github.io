aabeb18c05561fbce1744a0f47c4016927d7b233
Marilyn DeSesa <marilyn.desesa@gmail.com>
2017-11-02 16:46:14 -0700

    changed background color

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scrollToTop = $(this).scrollTop();
     if (scrollToTop - scrollLength > 50) {
-      var navHeight = $('.navbar').css('height');
-      $('.navbar').animate({top: '-' + navHeight}, 150);
+      var navHeight = $('header').css('header');
+      $('header').animate({top: '-' + navHeight}, 150);
       scrollLength = scrollToTop;
     } else if (scrollLength - scrollToTop > 50) {
-      $('.navbar').animate({top: '0px'}, 50);
+      $('header').animate({top: '0px'}, 50);
       scrollLength = scrollToTop;
     }
   });
