64759df593c70e799e1102bcffc1d6ade5d1523a
Marilyn DeSesa <marilyn.desesa@gmail.com>
2017-11-03 16:43:35 -0700

    added jquery fade effects

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+
   var scrollLength = 0
   $(window).scroll(function() {
     var scrollToTop = $(this).scrollTop();
